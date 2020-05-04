214d683a4c6ef1e2c5047b96eebe0aa3d16f8080
ceculbe <ceculbe@ilstu.edu>
2020-05-04 18:59:21 +0100

    output

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ~/LotMonitor
+cd home/pi//LotMonitor
 git pull --no-edit
-raspistill -w 640 -h 840 -n -t 10 -ifx emboss -o image.jpg
+raspistill -w 640 -h 840 -n -t 10 -ifx emboss -o /home/pi/LotMonitor/image.jpg
 java -cp . ProcessImage
 git add .
 git commit -m "output"
