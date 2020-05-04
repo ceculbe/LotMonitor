a9ad369e469faaaaf01b656f0c4ec9ddd2defbfd
ceculbe <ceculbe@ilstu.edu>
2020-05-04 21:08:06 +0100

    output

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ./home/pi/LotMonitor
+cd /home/pi/LotMonitor
 git pull --no-edit
 raspistill -w 640 -h 840 -n -t 10 -ifx emboss -o /home/pi/LotMonitor/image.jpg
 java -cp . ProcessImage
